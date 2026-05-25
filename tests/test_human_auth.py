ec420cf73a7618462c4657285eb5c9e198dd366c
Stephane Menard <steph@Stephanes-MacBook-Air.local>
2026-05-24 21:51:35 -0400

    test: verify HumanAuthError is exported from package public surface

--- tests/test_human_auth.py
+++ tests/test_human_auth.py
@@ -1,7 +1,12 @@
 from ibkr_core_mcp.exceptions import IBKRCoreError, HumanAuthError
+from ibkr_core_mcp import HumanAuthError as _HumanAuthErrorPublic
 
 
 def test_human_auth_error_is_ibkr_core_error():
     err = HumanAuthError("denied")
     assert isinstance(err, IBKRCoreError)
     assert str(err) == "denied"
+
+
+def test_human_auth_error_exported_from_package():
+    assert _HumanAuthErrorPublic is HumanAuthError
